187bc7722e5a910f6bb2ea2d31962c855857b085
Ataykhan Abeldiev <ataihanabeli@gmail.com>
2023-03-13 17:12:03 +0600

    first commit

---
+++ DZ/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import InputWithDelay from "./components/InputWithDelay";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Input with Delay</h1>
+      <InputWithDelay />
+    </div>
+  );
+}
+
+export default App;
